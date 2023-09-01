910f9105eaac92f8fb1bb4ad8cd5c5a636a12605
hmabed <hmabed@gmail.com>
2023-09-01 08:33:55 +0200

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,71 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import SignIn from './components/SignIn.vue'
+
 </script>
 
-<template>
+<template>    
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <router-link to="/">Go to Home</router-link>
+    <img alt="Logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <div class="wrapper" id="signOut">
+      <div><SignIn msg="User, please sign in !" /></div>
+      <label>email: </label><br>
+	    <input type="email" required v-model="email" placeholder="username@domain.tld"><br>
+	    <label>password: </label><br>
+	    <input type="password" required v-model="passwd" ><br>
+      <button v-on:click="register()">Sign Up</button>
+      <button v-on:click="login()">Sign In</button>
+      <p>
+      <label id="status"> You are not yet connected </label><br>  
+      </p>
     </div>
   </header>
-
+  
   <main>
-    <TheWelcome />
+    
   </main>
 </template>
 
-<style scoped>
+<script>
+
+
+
+
+export default {
+  methods: {  
+    //this method allows a new user to sign up the system. Once done, the user receives an email
+    //asking for account validation. Once the validation made the user is added to the system
+    async register(){
+      
+    },
+    //this method allows the already registred user to log in the system.
+    async login(){
+       
+    }
+  }  
+}
+</script>
+
+<style>
+@import './assets/base.css';
+
+header .hidden {
+    visibility: hidden;
+    overflow: hidden;
+    display: flex;
+    display:inline-block;
+    place-items: flex-start;
+    flex-wrap: wrap;
+}
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
 header {
   line-height: 1.5;
 }
@@ -27,21 +75,46 @@ header {
   margin: 0 auto 2rem;
 }
 
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
 @media (min-width: 1024px) {
-  header {
+  body {
     display: flex;
+    place-items: center;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+
+  header {
+    display: inline-block;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
   header .wrapper {
     display: flex;
+    display:inline-block;
     place-items: flex-start;
     flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
   }
 }
 </style>
