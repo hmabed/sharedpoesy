f4e5c933cbbcab10c4feb8434290b19c9ecd69c0
hmabed <hmabed@gmail.com>
2024-06-27 15:37:27 +0200

    new dev branch

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="I am more than the best !" />
+      <HelloWorld msg="Dev version of the code !" />
     </div>
   </header>
 
